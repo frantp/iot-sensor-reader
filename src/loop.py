f027a22dd73fa5cd858250c31e421be539d47909
Francisco Troncoso <ftroncoso@uvigo.es>
2019-08-06 13:46:02 +0200

    Improved configuration, included example file

--- src/loop.py
+++ src/loop.py
@@ -3,7 +3,7 @@
 import sys
 import os
 import time
-import yaml
+import toml
 import paho.mqtt.client as mqtt
 import importlib
 import traceback
@@ -20,8 +20,8 @@ def process(cfg, client, qos, measurement):
         sensor_id = sensor.split("_", 1)[0]
         try:
             sensor_module = importlib.import_module(f"sensors.{sensor}")
-            reader = getattr(sensor_module, "Reader")(**cfg[sensor]["init"])
-            timestamp, fields = reader.read(**cfg[sensor]["read"])
+            reader = getattr(sensor_module, "Reader")(**cfg[sensor])
+            timestamp, fields = reader.read()
             tags = {
                 "device": client._client_id,
                 "sensor": sensor_id
@@ -32,21 +32,15 @@ def process(cfg, client, qos, measurement):
 
 
 if __name__ == "__main__":
-    cfg_file = sys.argv[1] if len(sys.argv) >= 2 else "sreader.yml"
-    interval    = float(os.getenv("IOTSR_INTERVAL" , "5"))
-    device      =       os.getenv("IOTSR_DEVICE_ID", "test")
-    mqtt_host   =       os.getenv("IOTSR_MQTT_HOST", "localhost")
-    mqtt_port   =   int(os.getenv("IOTSR_MQTT_PORT", "1883"))
-    mqtt_qos    =   int(os.getenv("IOTSR_MQTT_QOS" , "2"))
-    measurement =       os.getenv("IOTSR_MQTT_MEAS", "sensors")
+    cfg_file = sys.argv[1] if len(sys.argv) >= 2 else "sreader.conf"
+    device    =     os.getenv("IOTSR_DEVICE_ID", "test")
+    mqtt_host =     os.getenv("IOTSR_MQTT_HOST", "localhost")
+    mqtt_port = int(os.getenv("IOTSR_MQTT_PORT", "1883"))
 
     print("Settings:")
     print(f" - Config file: '{cfg_file}'")
-    print(f" - Interval:    {interval} s")
     print(f" - Device ID:   '{device}'")
-    print(f" - MQTT Host:   '{mqtt_host}:{mqtt_port}'")
-    print(f" - MQTT QoS:    {mqtt_qos}")
-    print(f" - Measurement: '{measurement}'")
+    print(f" - MQTT broker: '{mqtt_host}:{mqtt_port}'")
 
     print("Connecting to MQTT broker")
 
@@ -56,9 +50,12 @@ if __name__ == "__main__":
     print("Starting loop")
 
     while True:
-        with open(cfg_file, "r") as f:
-            cfg = yaml.safe_load(f)
-        process(cfg, client, mqtt_qos, measurement)
+        cfg = toml.load(cfg_file)
+        interval = cfg.get("interval", 0)
+        qos = cfg.get("qos", 2)
+        measurement = cfg.get("measurement", "sensors")
+        sensors = cfg.get("sensors", {})
+        process(sensors, client, qos, measurement)
         if interval == 0:
             break
         time.sleep(interval)
