e778f96cb53496dd528c7966bd247a51309470a7
Francisco Troncoso <ftroncoso@uvigo.es>
2019-09-06 18:10:37 +0200

    Included firmware version in gdk101 output

--- src/sensors/gdk101.py
+++ src/sensors/gdk101.py
@@ -28,10 +28,13 @@ class Reader(BaseReader):
             self._address, _CMD_READ_MEASURING_VALUE_10M, 2)
         gint1m, gdec1m = self._sensor.read_i2c_block_data(
             self._address, _CMD_READ_MEASURING_VALUE_1M, 2)
+        firmm, firms = self._sensor.read_i2c_block_data(
+            self._address, _CMD_READ_FIRMWARE_VERSION, 2)
         return tm, OrderedDict([
             ("status", status),
             ("vibration", vibration),
             ("meastime", 60 * minutes + seconds),
             ("gamma10", gint10m + gdec10m / 100),
             ("gamma1", gint1m + gdec1m / 100),
+            ("firmware", "{}.{}".format(firmm, firms)),
         ])
