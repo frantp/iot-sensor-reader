4c306f9b230f45b10d09cdef7d212be270f92292
Francisco Troncoso <ftroncoso@uvigo.es>
2019-09-09 19:43:35 +0200

    Removed loop and MQTT, delegate to external tools

--- src/run.py
+++ src/run.py
@@ -5,16 +5,10 @@ import importlib
 import os
 import socket
 import sys
-import time
 import toml
 import traceback
 
 import RPi.GPIO as GPIO
-import paho.mqtt.client as mqtt
-
-
-def sync_time(interval):
-    return interval - time.time() % interval
 
 
 def format_msg(timestamp, measurement, tags, fields):
@@ -23,10 +17,8 @@ def format_msg(timestamp, measurement, tags, fields):
     return "{},{} {} {}".format(measurement, tstr, fstr, timestamp)
 
 
-def run(cfg, client, qos, measurement, pin_list=[]):
-    device_id = client._client_id.decode("utf-8")
+def run(cfg, device_id, measurement, pin_list=[]):
     for driver_id in cfg:
-        print("Running driver '{}'".format(driver_id))
         try:
             GPIO.output(pin_list, GPIO.HIGH)
             dcfg = cfg[driver_id]
@@ -42,9 +34,7 @@ def run(cfg, client, qos, measurement, pin_list=[]):
                 ("device", device_id),
                 ("sensor", driver_id)
             ])
-            topic = "{}/{}/{}".format(measurement, device_id, driver_id)
-            payload = format_msg(timestamp, measurement, tags, fields)
-            client.publish(topic, payload, qos)
+            print(format_msg(timestamp, measurement, tags, fields))
         except:
             traceback.print_exc()
 
@@ -54,35 +44,22 @@ if __name__ == "__main__":
         print("Usage: {} <cfg_file>".format(sys.argv[0]))
     cfg_file  = sys.argv[1]
 
-    print("Reading configuration")
+    # Read configuration
     cfg = toml.load(cfg_file)
     interval = int(cfg.get("interval", "5"))
-    device = cfg.get("device") or socket.gethostname()
+    device_id = cfg.get("device") or socket.gethostname()
     measurement = cfg.get("measurement", "data")
-    cfg_mqtt = cfg.get("mqtt", {})
-    mqtt_host = cfg_mqtt.get("host", "localhost")
-    mqtt_port = int(cfg_mqtt.get("port", "1883"))
-    mqtt_qos = cfg_mqtt.get("qos", 2)
     cfg_drivers = cfg.get("drivers", {})
 
-    print(f"Connecting to MQTT broker at '{mqtt_host}:{mqtt_port}'")
-    client = mqtt.Client(device, clean_session=False)
-    client.connect(mqtt_host, mqtt_port)
-    client.loop_start()
-
-    print("Configuring GPIO")
+    # Configure GPIO
     PIN_STR = "ACTIVATION_PIN"
     GPIO.setmode(GPIO.BCM)
     pin_list = [cfg_drivers[driver_id][PIN_STR]
         for driver_id in cfg_drivers if PIN_STR in cfg_drivers[driver_id]]
     GPIO.setup(pin_list, GPIO.OUT)
 
-    print("Starting readings")
+    # Run drivers
     try:
-        while True:
-            print("Waiting {} seconds".format(sync_time(interval)))
-            time.sleep(sync_time(interval))
-            run(cfg_drivers, client, mqtt_qos, measurement, pin_list)
+        run(cfg_drivers, device_id, measurement, pin_list)
     finally:
         GPIO.cleanup()
-        client.disconnect()
