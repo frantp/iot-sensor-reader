c9bb0db37cb0c5cf5046ddc22d423be19ec849f3
Francisco Troncoso <ftroncoso@uvigo.es>
2019-09-11 13:09:54 +0200

    Returned to continuous execution (but without MQTT)

--- src/run.py
+++ src/run.py
@@ -5,9 +5,16 @@ from collections import OrderedDict
 import importlib
 import RPi.GPIO as GPIO
 import sys
+import time
 import toml
 import traceback
 
+
+PIN_STR = "ACTIVATION_PIN"
+
+
+def sync_time(interval):
+    return interval - time.time() % interval if interval > 0 else 0
 
 def format_msg(timestamp, measurement, fields):
     fstr = ",".join(["{}={}".format(k, v) for k, v in fields.items()])
@@ -15,12 +22,6 @@ def format_msg(timestamp, measurement, fields):
 
 
 def run(cfg):
-    PIN_STR = "ACTIVATION_PIN"
-    GPIO.setmode(GPIO.BCM)
-    pin_list = [driver_cfg[PIN_STR]
-        for driver_id in cfg
-        for driver_cfg in cfg[driver_id] if PIN_STR in driver_cfg]
-    GPIO.setup(pin_list, GPIO.OUT, initial=GPIO.HIGH)
     for driver_id in cfg:
         try:
             for dcfg in cfg[driver_id]:
@@ -46,7 +47,23 @@ if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("Usage: {} <cfg_file>".format(sys.argv[0]))
     cfg_file  = sys.argv[1]
+
+    # Read configuration
+    cfg = toml.load(cfg_file)
+    interval = int(cfg.get("interval", "0"))
+    drivers_cfg = cfg.get("drivers", {})
+
+    # Configure GPIO
+    GPIO.setmode(GPIO.BCM)
+    pin_list = [driver_cfg[PIN_STR]
+        for driver_id in drivers_cfg
+        for driver_cfg in drivers_cfg[driver_id] if PIN_STR in driver_cfg]
+    GPIO.setup(pin_list, GPIO.OUT, initial=GPIO.HIGH)
+
+    # Run drivers
     try:
-        run(toml.load(cfg_file))
+        while True:
+            time.sleep(sync_time(interval))
+            run(drivers_cfg)
     finally:
         GPIO.cleanup()
