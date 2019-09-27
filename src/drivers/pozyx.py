acc4ddd9b00f58f51f931e3eef018e5c96c88138
Francisco Troncoso <ftroncoso@uvigo.es>
2019-09-27 17:02:18 +0200

    [pozyx] Added option to use I2C

--- src/drivers/pozyx.py
+++ src/drivers/pozyx.py
@@ -5,14 +5,14 @@ import pypozyx
 
 
 class Driver(DriverBase):
-    def __init__(self, port=None):
+    def __init__(self, i2c=False, port=None):
         super().__init__()
-        self._serial = pypozyx.PozyxSerial(
+        self._sensor = pypozyx.PozyxI2C() if i2c else pypozyx.PozyxSerial(
             port or pypozyx.get_first_pozyx_serial_port())
 
     def run(self):
         position = pypozyx.Coordinates()
-        self._serial.doPositioning(position)
+        self._sensor.doPositioning(position)
         return [(self.sid(), int(time.time() * 1e9), OrderedDict([
             ("pos_x", position.x),
             ("pos_y", position.y),
