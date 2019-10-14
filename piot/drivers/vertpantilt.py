e9862c88e4c41248bfc43905c111a1fa866c3a32
Francisco Troncoso <ftroncoso@uvigo.es>
2019-10-14 12:57:35 +0200

    [vertpantilt] Added read interval parameter

--- piot/drivers/vertpantilt.py
+++ piot/drivers/vertpantilt.py
@@ -15,13 +15,15 @@ class Driver(SMBusDriver):
 
 
     def __init__(self, address, bus=1, movement=None, drivers=None,
-        interval=0, polling_interval=0.1, retry_interval=0.5, check_move=True):
+        interval=0, read_interval=0.5, polling_interval=0.1,
+        retry_interval=0.5, check_move=True):
         super().__init__(bus)
         self._address = address
         self._busnum = bus
         self._movement = movement
         self._drivers = drivers
         self._interval = interval
+        self._read_interval = read_interval
         self._polling_interval = polling_interval
         self._retry_interval = retry_interval
         self._check_move = check_move
@@ -32,7 +34,7 @@ class Driver(SMBusDriver):
             for pan in _get_range(self._movement["pan"]):
                 for tilt in _get_range(self._movement["tilt"]):
                     self._move(vert, pan, tilt)
-                    time.sleep(0.1)
+                    time.sleep(self._read_interval)
                     vert, pan, tilt, flags, bt1, bt2 = self._read()
                     state = OrderedDict([
                         ("vert"    , vert),
