5755665eabedcd3c69f63109afda97f212e03d8e
Francisco Troncoso <ftroncoso@uvigo.es>
2019-11-15 12:17:12 +0100

    [ssd1306] Improved displayed information

--- piot/drivers/ssd1306.py
+++ piot/drivers/ssd1306.py
@@ -17,29 +17,29 @@ class Driver(DriverBase):
 
 
     def run(self):
-        # Initialize library.
+        # Initialize library
         self._disp.begin()
         
-        # Clear display.
+        # Clear display
         self._disp.clear()
         self._disp.display()
         
-        # Create blank image for drawing.
-        # Make sure to create image with mode '1' for 1-bit color.
+        # Create blank image for drawing
+        # Make sure to create image with mode '1' for 1-bit color
         width = self._disp.width
         height = self._disp.height
         image = Image.new('1', (width, height))
         
-        # Get drawing object to draw on image.
+        # Get drawing object to draw on image
         draw = ImageDraw.Draw(image)
         
-        # Draw a black filled box to clear the image.
+        # Draw a black filled box to clear the image
         draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-        # Load default font.
+        # Load default font
         font = ImageFont.load_default()
 
-        # Retrieve values.
+        # Retrieve values
         timestr = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         try:
             ssid = subprocess.check_output("/sbin/iwgetid -r", shell = True) \
@@ -47,15 +47,16 @@ class Driver(DriverBase):
         except:
             ssid = "---"
         device_id = socket.gethostname()
+        device_ip = socket.gethostbyname(device_id + ".local")
 
         # Write text
         t, l = -2, 0
-        draw.text((l, t + 0),  " "       + timestr,     font=font, fill=255)
-        draw.text((l, t + 8),  "---------------------", font=font, fill=255)
-        draw.text((l, t + 16), "SSID:  " + ssid,        font=font, fill=255)
-        draw.text((l, t + 24), "DevID: " + device_id,   font=font, fill=255)
+        draw.text((l, t + 0),  " "      + timestr,   font=font, fill=255)
+        draw.text((l, t + 8),  "Name: " + device_id, font=font, fill=255)
+        draw.text((l, t + 16), "IP:   " + device_ip, font=font, fill=255)
+        draw.text((l, t + 24), "SSID: " + ssid,      font=font, fill=255)
 
-        # Display image.
+        # Display image
         self._disp.image(image)
         self._disp.display()
 
