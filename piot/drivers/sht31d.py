9bafac3765f3ada501a99ae1b7621d97fa28ff8f
Francisco Troncoso Pastoriza <frantp90@gmail.com>
2020-05-27 13:25:23 +0200

    Added sht31d

---
+++ piot/drivers/sht31d.py
@@ -0,0 +1,21 @@
+from collections import OrderedDict
+import time
+
+from ..core import I2CDriver
+import board
+import busio
+from adafruit_sht31d import SHT31D
+
+
+class Driver(I2CDriver):
+    def __init__(self, address=0x77):
+        super().__init__()
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self._sensor = SHT31D(i2c, address=address)
+
+
+    def run(self):
+        return [(self.sid(), int(time.time() * 1e9), OrderedDict([
+            ("temperature", self._sensor.temperature),
+            ("humidity",    self._sensor.relative_humidity)
+        ]))]
