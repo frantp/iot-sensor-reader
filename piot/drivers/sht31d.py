223f6651766c39c06a59de5139ec9881a15bbed0
Francisco Troncoso Pastoriza <frantp90@gmail.com>
2020-05-28 22:19:26 +0200

    [sht31d] Fixed default address

--- piot/drivers/sht31d.py
+++ piot/drivers/sht31d.py
@@ -8,11 +8,10 @@ from adafruit_sht31d import SHT31D
 
 
 class Driver(I2CDriver):
-    def __init__(self, address=0x77):
+    def __init__(self, address=0x44):
         super().__init__()
         i2c = busio.I2C(board.SCL, board.SDA)
         self._sensor = SHT31D(i2c, address=address)
-
 
     def run(self):
         return [(self.sid(), int(time.time() * 1e9), OrderedDict([
