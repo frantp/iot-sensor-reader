64a30669a03e03422829ec6e8910ac4627e38798
Francisco Troncoso Pastoriza <frantp90@gmail.com>
2020-05-31 18:25:25 +0200

    [ssd1306] Fixed slow IP readings.

--- piot/outputs/ssd1306.py
+++ piot/outputs/ssd1306.py
@@ -1,9 +1,10 @@
 import datetime
 import socket
 import subprocess
+from threading import Timer
 import time
 
-from ..core import DriverBase
+from ..core import DriverBase, TAG_ERROR, TAG_NOLIB
 from PIL import Image, ImageDraw, ImageFont
 from Adafruit_SSD1306 import SSD1306_128_32, SSD1306_128_64
 
@@ -15,6 +16,9 @@ class Driver(DriverBase):
         self._buffer = {}
 
     def run(self, driver_id, ts, fields, tags):
+        if not fields:
+            return
+
         # Initialize library
         self._disp.begin()
 
@@ -40,19 +44,20 @@ class Driver(DriverBase):
         # Retrieve values
         timestr = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         try:
-            ssid = subprocess.check_output("/sbin/iwgetid -r", shell=True) \
-                .decode("utf-8")
-        except:
+            ssid = subprocess.check_output(["/sbin/iwgetid", "-r"]) \
+                .decode("utf-8").strip()
+        except Exception:
             ssid = "---"
-        device_id = socket.gethostname()
-        device_ip = socket.gethostbyname(device_id + ".local")
+        devid = socket.gethostname()
+        devip = subprocess.check_output(["hostname", "-I"]) \
+            .decode("utf-8").strip()
 
         # Write text
         t, l = -2, 0
-        draw.text((l, t + 0),  " "      + timestr,   font=font, fill=255)
-        draw.text((l, t + 8),  "Name: " + device_id, font=font, fill=255)
-        draw.text((l, t + 16), "IP:   " + device_ip, font=font, fill=255)
-        draw.text((l, t + 24), "SSID: " + ssid,      font=font, fill=255)
+        draw.text((l, t + 0),  " "      + timestr, font=font, fill=255)
+        draw.text((l, t + 8),  "Name: " + devid,   font=font, fill=255)
+        draw.text((l, t + 16), "IP:   " + devip,   font=font, fill=255)
+        draw.text((l, t + 24), "SSID: " + ssid,    font=font, fill=255)
 
         # Display image
         self._disp.image(image)
