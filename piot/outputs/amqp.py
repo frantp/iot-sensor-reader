85587666b51085052e9a7ade95d1fb24a995d82a
Francisco Troncoso Pastoriza <frantp90@gmail.com>
2020-06-03 16:33:06 +0200

    [amqp] Option to buffer messages.

--- piot/outputs/amqp.py
+++ piot/outputs/amqp.py
@@ -1,11 +1,12 @@
-import os
+from queue import Queue, Empty, Full
 
 from ..core import DriverBase, format_msg
 import pika
 
 
 class Driver(DriverBase):
-    def __init__(self, exchange, queue, routing_key=None, *args, **kwargs):
+    def __init__(self, exchange, queue, routing_key=None, buffer_maxsize=None,
+                 *args, **kwargs):
         super().__init__()
         self._args = args
         self._kwargs = kwargs
@@ -21,17 +22,38 @@ class Driver(DriverBase):
                 queue=queue,
                 routing_key=self._routing_key
             )
+        self._buffer = Queue(buffer_maxsize) \
+            if buffer_maxsize is not None else None
 
     def run(self, driver_id, ts, fields, tags):
         if not fields:
             return
         msg = format_msg(ts, driver_id, tags, fields)
-        with pika.BlockingConnection(
-             pika.ConnectionParameters(*self._args, **self._kwargs)) as c:
-            channel = c.channel()
-            channel.basic_publish(
-                exchange=self._exchange,
-                routing_key=self._routing_key,
-                body=msg,
-                properties=pika.BasicProperties(delivery_mode=2)
-            )
+        try:
+            with pika.BlockingConnection(
+                 pika.ConnectionParameters(*self._args, **self._kwargs)) as c:
+                channel = c.channel()
+                self._publish(channel, msg)
+                # Flush buffer
+                if self._buffer is not None:
+                    try:
+                        while True:
+                            msg = self._buffer.get_nowait()
+                            self._publish(channel, msg)
+                    except Empty:
+                        pass
+        except pika.exceptions.AMQPError:
+            # Add to buffer
+            if self._buffer is not None:
+                try:
+                    self._buffer.put_nowait(msg)
+                except Full:
+                    pass
+
+    def _publish(self, channel, msg):
+        channel.basic_publish(
+            exchange=self._exchange,
+            routing_key=self._routing_key,
+            body=msg,
+            properties=pika.BasicProperties(delivery_mode=2)
+        )
