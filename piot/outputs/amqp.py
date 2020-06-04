8d3d3b35cab0ce4132190986512e82181d3f6b1b
Francisco Troncoso Pastoriza <frantp90@gmail.com>
2020-06-04 02:13:03 +0200

    [amqp] Correct error handling and reconnection.

--- piot/outputs/amqp.py
+++ piot/outputs/amqp.py
@@ -11,19 +11,11 @@ class Driver(DriverBase):
         self._args = args
         self._kwargs = kwargs
         self._exchange = exchange
+        self._queue = queue
         self._routing_key = routing_key or queue
-        with pika.BlockingConnection(
-             pika.ConnectionParameters(*self._args, **self._kwargs)) as c:
-            channel = c.channel()
-            channel.exchange_declare(exchange=exchange, durable=True)
-            channel.queue_declare(queue=queue, durable=True)
-            channel.queue_bind(
-                exchange=exchange,
-                queue=queue,
-                routing_key=self._routing_key
-            )
         self._buffer = Queue(buffer_maxsize) \
             if buffer_maxsize is not None else None
+        self._declared = False
 
     def run(self, driver_id, ts, fields, tags):
         if not fields:
@@ -50,7 +42,19 @@ class Driver(DriverBase):
                 except Full:
                     pass
 
+    def _declare(self, channel):
+        if not self._declared:
+            channel.exchange_declare(exchange=self._exchange, durable=True)
+            channel.queue_declare(queue=self._queue, durable=True)
+            channel.queue_bind(
+                exchange=self._exchange,
+                queue=self._queue,
+                routing_key=self._routing_key
+            )
+            self._declared = True
+
     def _publish(self, channel, msg):
+        self._declare(channel)
         channel.basic_publish(
             exchange=self._exchange,
             routing_key=self._routing_key,
