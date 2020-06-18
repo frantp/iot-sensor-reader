06e704de3450a8465678691d11b5c110bd8b2af5
Francisco Troncoso Pastoriza <frantp90@gmail.com>
2020-06-18 21:35:35 +0200

    Added bme680_bsec input

---
+++ piot/inputs/bme680_bsec/__init__.py
@@ -0,0 +1,39 @@
+from collections import OrderedDict, deque
+import json
+import os
+import subprocess as sp
+import time
+import threading
+
+from ...core import DriverBase
+
+
+EXE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bsec_bme680")
+
+
+def _pipe(fh, q):
+    for line in fh:
+        q.append(json.loads(line.strip(), object_pairs_hook=OrderedDict))
+    fh.close()
+    q.put(None)
+
+
+class Driver(DriverBase):
+    def __init__(self):
+        self._queue = deque(maxlen=1)
+        self._proc = sp.Popen([EXE], text=True,
+                              stdout=sp.PIPE, stderr=sp.DEVNULL)
+        self._thread = threading.Thread(target=_pipe,
+                                        args=(self._proc.stdout, self._queue))
+        self._thread.start()
+
+    def run(self):
+        try:
+            res = self._queue.popleft()
+        except IndexError:
+            res = None
+        return [(self.sid(), time.time_ns(), res)]
+
+    def close(self):
+        self._proc.terminate()
+        self._thread.join()
